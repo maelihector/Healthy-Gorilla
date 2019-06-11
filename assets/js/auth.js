5410fedefd618a38f8064f8821e84179da4f71bc
mhector <maelihector@gmail.com>
2019-06-11 18:20:04 -0500

    Fix firebase new user registration, fix firebase logout, and localStorage settings.

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,295 +1,138 @@
-// Add Firebase to HealthyGorillaApp web app
-var firebaseConfig = {
-  apiKey: "AIzaSyCPIfGudl6LOhVtt2vIM-lDUTdHa33WPLI",
-  authDomain: "healthygorilla.firebaseapp.com",
-  databaseURL: "https://healthygorilla.firebaseio.com",
-  projectId: "healthygorilla",
-  storageBucket: "healthygorilla.appspot.com",
-  messagingSenderId: "977887423528",
-  appId: "1:977887423528:web:c9dbade3d29fb137"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+$(document).ready(function () {
 
-// Firebase variables
-var database = firebase.database();
-var user;
-var ref = firebase.database().ref();
-var userId = JSON.parse(localStorage.getItem("UID"));
+  // Add Firebase to HealthyGorillaApp web app
+  var firebaseConfig = {
+    apiKey: "AIzaSyCPIfGudl6LOhVtt2vIM-lDUTdHa33WPLI",
+    authDomain: "healthygorilla.firebaseapp.com",
+    databaseURL: "https://healthygorilla.firebaseio.com",
+    projectId: "healthygorilla",
+    storageBucket: "healthygorilla.appspot.com",
+    messagingSenderId: "977887423528",
+    appId: "1:977887423528:web:c9dbade3d29fb137"
+  };
 
-// Console log the current user
-console.log("The current UID is: " + userId);
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
-ref.on("value", function(snapshot) {
-}, function (error) {
-console.log("Error: " + error.code);
-});
-
-var refId = firebase.database().ref("users/" + userId);
-
-// Create a new user
-$("#register-btn").on("click", function(){
-    var email = $("#register_email").val().trim();
-    var password = $("#register_password").val().trim();
-    var displayName = $("#register_name").val().trim();
-    
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then(function(user){
-        return user.updateProfile({
-            displayName: displayName
-        });
-    }).then(
-        $( '#register-form' ).each(function(){
-        this.reset();
-    })).catch(function(error) {
-        console.log(error.code);
-        console.log(error.message);
-        return;
+  // Function to handle firebase registration submit
+  function handleUserRegistration() {
+    event.preventDefault();
+    var email = $("#register_email").val();
+    var password = $("#register_password").val();
+    var displayName = $("#register_name").val();
+    if (email.length < 4) {
+      alert('Please enter an email address.');
+      return;
+    }
+    if (password.length < 4) {
+      alert('Please enter a password.');
+      return;
+    }
+    // Create new user with email and password
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // [START_EXCLUDE]
+      if (errorCode == 'auth/weak-password') {
+        alert('The password is too weak.');
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error);
+      // [END_EXCLUDE]
     });
 
-    isLoggedIn();
+    localStorage.setItem("displayName", displayName);
+    console.log(displayName);
 
-});
+    var uid = localStorage.getItem("uid");
+
+    firebase.database().ref('users/' + displayName).set({
+      displayName: displayName,
+      email: email,
+      uid: uid
+    });
+
+    $('.modal').hide();
 
 
-// Login existing user
-$("#login-btn").on("click", function(){
+
+  }
+
+  // Listen for auth state changes
+  function initiateAPP() {
+    firebase.auth().onAuthStateChanged(function (user) {
+      if (user) {
+        // If user is signed in:
+        var displayName = localStorage.getItem('displayName');
+        var email = user.email;
+        var uid = user.uid;
+        console.log(user, email, uid);
+        $("#helloName").show();
+        $("#helloName").html("<h2>Hello " + displayName + "!</h2>");
+        $("#signInBtn").hide();
+        $("#signOutBtn").show();
+        return (user);
+      } else {
+        $("#signInBtn").show();
+        $("#signOutBtn").hide();
+      }
+    })
+  }
+
+  initiateAPP();
+  //  EVENTS
+
+  // Event handler for registration submit
+  $("#register-btn").on("click", function (event) {
+    handleUserRegistration();
+  });
+
+  // Allow users to sign out. 
+  $("#signOutBtn").on("click", function (event) {
+    event.preventDefault();
+    firebase.auth().signOut().then(function () {
+      $('#login-modal').css('display:none');
+      console.log("Logged out!");
+      // Clear absolutely everything stored in localStorage using localStorage.clear()
+      localStorage.clear();
+      location.reload(true);
+      $("#signInBtn").show();
+    });
+  });
+
+  // Login existing user
+  $("#login-btn").on("click", function (event) {
+    event.preventDefault();
     var email = $("#login_email").val().trim();
     var password = $("#login_password").val().trim();
     var user = firebase.auth().signInWithEmailAndPassword(email, password)
-    .catch(function(error) {
-        if(error.code === "auth/invalid-email"){
-        $('#login-modal').modal('open');
-        alert("Incorrect email format.");
-        console.log(error.code);
-        console.log(error.message);
-        return;
+      .catch(function (error) {
+        if (error.code === "auth/invalid-email") {
+          $('#login-modal').modal('open');
+          alert("Incorrect email format.");
+          console.log(error.code);
+          console.log(error.message);
+          return;
         }
-        if(error.code === "auth/user-not-found"){
-        $('#login-modal').modal('open');
-        alert("Invalid email or password");
-        console.log(error.code);
-        console.log(error.message);
-        return;
-        }else{
-        $('#login-modal').modal('close');
-        }
-        
-    });
-
-    isLoggedIn();
-
-});
-
-
-// Allow users to sign out. 
-$("#signOutBtn").on("click", function(){
-    firebase.auth().signOut().then(function() {
-        console.log("Logged out!")
-        localStorage.removeItem('UID');
-        isLoggedIn();
-
-    }, function(error) {
-        console.log(error.code);
-        console.log(error.message);
-    });
-	});
-
-
-function isLoggedIn(){
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-        console.log("User is logged in.");
-            user = firebase.auth().currentUser;
-            userId = firebase.auth().currentUser.uid;
-            localStorage.setItem("UID", JSON.stringify(userId));
-                console.log(user);
-                console.log(user.displayName);
-                console.log(userId);
-                $("#helloName").show();
-                $("#helloName").html("Hello, " + user.displayName + "!");
-                $("#signInBtn").hide();
-                $("#signOutBtn").show();
+        if (error.code === "auth/user-not-found") {
+          $('#login-modal').modal('open');
+          alert("Invalid email or password");
+          console.log(error.code);
+          console.log(error.message);
+          return;
         } else {
-        $("#signInBtn").show();
-        $("#signOutBtn").hide();
-        $("#helloName").empty();
-        $("#helloName").hide();
-        console.log("No user logged in!");
+          $('#login-modal').modal('close');
         }
-    });
-}
- 
-$(function() {
-    
-    var $formLogin = $('#login-form');
-    var $formLost = $('#lost-form');
-    var $formRegister = $('#register-form');
-    var $divForms = $('#div-forms');
-    var $modalAnimateTime = 300;
-    var $msgAnimateTime = 150;
-    var $msgShowTime = 2000;
 
-    $("form").submit(function () {
-        switch(this.id) {
-            case "login-form":
-                var $lg_email=$('#login_email').val();
-                var $lg_password=$('#login_password').val();
-                if ($lg_email == "ERROR") {
-                    msgChange($('#div-login-msg'), $('#icon-login-msg'), $('#text-login-msg'), "error", "glyphicon-remove", "Login error");
-                } else {
-                    msgChange($('#div-login-msg'), $('#icon-login-msg'), $('#text-login-msg'), "success", "glyphicon-ok", "Login OK");
-                }
-                return false;
-                break;
-            case "lost-form":
-                var $ls_email=$('#lost_email').val();
-                if ($ls_email == "ERROR") {
-                    msgChange($('#div-lost-msg'), $('#icon-lost-msg'), $('#text-lost-msg'), "error", "glyphicon-remove", "Send error");
-                } else {
-                    msgChange($('#div-lost-msg'), $('#icon-lost-msg'), $('#text-lost-msg'), "success", "glyphicon-ok", "Send OK");
-                }
-                return false;
-                break;
-            case "register-form":
-                // var $rg_name=$('#register_name').val();
-                var $rg_email=$('#register_email').val();
-                var $rg_password=$('#register_password').val();
-                if ($rg_email == "ERROR") {
-                    msgChange($('#div-register-msg'), $('#icon-register-msg'), $('#text-register-msg'), "error", "glyphicon-remove", "Register error");
-                } else {
-                    msgChange($('#div-register-msg'), $('#icon-register-msg'), $('#text-register-msg'), "success", "glyphicon-ok", "Register OK");
-                }
-                return false;
-                break;
-            default:
-                return false;
-        }
-        return false;
-    });
-    
-    $('#login_register_btn').click( function () { modalAnimate($formLogin, $formRegister) });
-    $('#register_login_btn').click( function () { modalAnimate($formRegister, $formLogin); });
-    $('#login_lost_btn').click( function () { modalAnimate($formLogin, $formLost); });
-    $('#lost_login_btn').click( function () { modalAnimate($formLost, $formLogin); });
-    $('#lost_register_btn').click( function () { modalAnimate($formLost, $formRegister); });
-    $('#register_lost_btn').click( function () { modalAnimate($formRegister, $formLost); });
-    
-    function modalAnimate ($oldForm, $newForm) {
-        var $oldH = $oldForm.height();
-        var $newH = $newForm.height();
-        $divForms.css("height",$oldH);
-        $oldForm.fadeToggle($modalAnimateTime, function(){
-            $divForms.animate({height: $newH}, $modalAnimateTime, function(){
-                $newForm.fadeToggle($modalAnimateTime);
-            });
-        });
-    }
-    
-    function msgFade ($msgId, $msgText) {
-        $msgId.fadeOut($msgAnimateTime, function() {
-            $(this).text($msgText).fadeIn($msgAnimateTime);
-        });
-    }
-    
-    function msgChange($divTag, $iconTag, $textTag, $divClass, $iconClass, $msgText) {
-        var $msgOld = $divTag.text();
-        msgFade($textTag, $msgText);
-        $divTag.addClass($divClass);
-        $iconTag.removeClass("glyphicon-chevron-right");
-        $iconTag.addClass($iconClass + " " + $divClass);
-        setTimeout(function() {
-            msgFade($textTag, $msgOld);
-            $divTag.removeClass($divClass);
-            $iconTag.addClass("glyphicon-chevron-right");
-            $iconTag.removeClass($iconClass + " " + $divClass);
-  		}, $msgShowTime);
-    }
+        localStorageUserId = localStorage.getItem("uid", JSON.stringify(userId));
+        console.log(localStorageUserId);
+        // userId = firebase.auth().currentUser.uid;
+
+      });
+  });
+
+
+
 });
-
-
-
-
-// $("#addInfoBtn").on("click", function(evt){
-// 	evt.preventDefault();
-
-// 	 var userFirstName = $("#firstNameInput").val().trim();
-// 	 var userLastName = $("#lastNameInput").val().trim();
-// 	 var userAddress = $("#addressInput").val().trim();
-// 	 var userPhone = $("#phoneNumberInput").val().trim();
-// 	 var userEmail = $("#emailInput").val().trim();
-// 	 var modified = new Date();
-// 	 userId = JSON.parse(localStorage.getItem("UID"));
-// 	 console.log(userFirstName);
-// 	 console.log(userLastName);
-// 	 console.log(userAddress);
-// 	 console.log(userPhone);
-// 	 console.log(userEmail);
-// 	 console.log(modified);
-// 	 console.log(userId);
-
-// 	 var acctInfo = {
-// 	 		userId: userId,
-// 		    firstName: userFirstName,
-// 		    lastName: userLastName,
-// 		    address: userAddress,
-// 		    phone: userPhone,
-// 		    email: userEmail,
-// 		    lastModified: modified
-// 	 };
-
-// 	 if(userId === null){
-// 	 	alert("You do not have an account!");
-// 	 	 userFirstName = $("#firstNameInput").val("");
-// 		 userLastName = $("#lastNameInput").val("");
-// 		 userAddress = $("#addressInput").val("");
-// 		 userPhone = $("#phoneNumberInput").val("");
-// 		 userEmail = $("#emailInput").val("");
-// 	 	return;
-// 	 }
-	 	
-
-// 	 database.ref('users/' + userId).update(acctInfo);
-// 	 userFirstName = $("#firstNameInput").val("");
-// 	 userLastName = $("#lastNameInput").val("");
-// 	 userAddress = $("#addressInput").val("");
-// 	 userPhone = $("#phoneNumberInput").val("");
-// 	 userEmail = $("#emailInput").val("");
-// 	 alert("You updated your account information!");
-
-
-
-// 	 appendToHTML();
-// });
-
-
-// function appendToHTML(){
-// 		var userDataRef = firebase.database().ref("users/" + userId).orderByKey();
-// 		userDataRef.once("value").then(function(snapshot) {
-// 		snapshot.forEach(function(childSnapshot) {
-// 		  var key = childSnapshot.key;
-// 		  // console.log(key);
-// 		  var childData = childSnapshot.val();              
-// 		  console.log(key + ": " + childData);
-// 		  // var name_val = childSnapshot.val().firstName;
-// 		  // console.log(name_val);
-// 		  // var id_val = childSnapshot.val().lastName;
-// 		  // console.log(id_val);
-	
-// 		  // $("#name").append(name_val);
-// 		  // $("#id").append(id_val);
-
-// 		  });
-// 		});
-
-// 		hideForm();
-	
-// 	}
-
-// function hideForm(){
-// 	$("#update_account").hide();
-// }
-
-// function showForm(){
-// 	$("#update_account").show();
-// }
