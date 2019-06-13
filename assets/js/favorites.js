abc44f4e9d228904650320c188ba190e61669c37
mhector <maelihector@gmail.com>
2019-06-13 18:44:51 -0500

    Add javaScript to update firebase database with user added favorites and removed favorites from index.html.

--- assets/js/favorites.js
+++ assets/js/favorites.js
@@ -3,57 +3,46 @@ $(document).ready(function () {
   // Function to add user favorites to database
   $(document).on('click', '#blankHeartImage', function () {
 
-    console.log("Added to favorites!");
-    // Grab the parent element of the clicked image
-    var favoriteCollectionItem = $(this).parent();
-    console.log(favoriteCollectionItem);
-    // Grab the information we want to store in the database
-    var favoriteObject = favoriteCollectionItem[0].parentElement.children;
-    console.log(favoriteObject);
-    var name = favoriteObject[0].innerHTML;
-    console.log(name);
-    var website = favoriteObject[0].href;
-    console.log(website);
-    var address = favoriteObject[1].innerHTML;
-    console.log(address);
-    var phoneNumber = favoriteObject[2].innerHTML;
-    console.log(phoneNumber);
     // Authorize current user
     var currentUserUid = firebase.auth().currentUser.uid;
     console.log(currentUserUid);
 
+    // Grab the parent element of the clicked image
+    var favoriteCollectionItem = $(this).parent();
+
+    // Grab the information we want to store in the database
+    var favoriteObject = favoriteCollectionItem[0].parentElement.children;
+    var name = favoriteObject[0].innerHTML;
+    var website = favoriteObject[0].href;
+    var address = favoriteObject[1].innerHTML;
+    var phoneNumber = favoriteObject[2].innerHTML;
+
     // Create a new favorite entry
     var addFavorite = {
-      name: name,
       website: website,
       address: address,
       phoneNumber: phoneNumber
     };
 
-    // Get a key for a new favorite entry
-    var newFavoriteKey = firebase.database().ref().child('favorites').push().key;
-    // Write the new post's data under users
     var updates = {};
-    updates['/users/' + currentUserUid + '/favorites/' + newFavoriteKey] = addFavorite;
 
-    return firebase.database().ref().update(updates);
-
-
-
-
+    // retrieve and store images to display when favorited
+    var state = $(this).attr("state");
+    // Toggle between adding and removing favarites
+    if (state === "falseFavorite") {
+      $(this).attr("src", $(this).attr("trueFavorite"));
+      $(this).attr("state", "trueFavorite");
+      // Path to save user favorite
+      updates['/users/' + currentUserUid + '/favorites/' + name] = addFavorite;
+      // Save user favorite
+      return firebase.database().ref().update(updates);
+    } else if (state === "trueFavorite") {
+      $(this).attr("src", $(this).attr("falseFavorite"));
+      $(this).attr("state", "falseFavorite");
+      // Remove favorite 
+      return firebase.database().ref('/users/' + currentUserUid + '/favorites/' + name).remove();
+    }
   });
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
