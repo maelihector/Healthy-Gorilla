c29154d68bd4d2ef5263c1a53304212608397c5a
mhector <maelihector@gmail.com>
2019-06-19 15:09:24 -0500

    Add cuisine images to show with user favorite cards. Edit CSS.

--- assets/js/account.js
+++ assets/js/account.js
@@ -14,11 +14,88 @@ $(document).ready(function () {
 
   // Function to create favorite cards
   function createFavoriteCards(favorites) {
+    // Add appropriate image
+    let cuisineImage;
+    switch (favorites.cuisine) {
+      case 'Mexican':
+        cuisineImage = './assets/images/mexican1.jpg';
+        break;
+      case 'American':
+        cuisineImage = './assets/images/american2.jpg';
+        break;
+      case 'Pizza':
+        cuisineImage = './assets/images/pizza.jpg';
+        break;
+      case 'Ethiopian/Eritrean':
+        cuisineImage = './assets/images/ethiopian.jpg';
+        break;
+      case 'Eclectic (Varied)':
+        cuisineImage = './assets/images/eclectic.png';
+        break;
+      case 'Macrobiotic':
+        cuisineImage = './assets/images/macrobiotic.jpg';
+        break;
+      case 'Chinese':
+        cuisineImage = './assets/images/chinese.jpg';
+        break;
+      case 'European':
+        cuisineImage = './assets/images/european1.jpg';
+        break;
+      case 'Tex-Mex':
+        cuisineImage = './assets/images/tex-mex.jpg';
+        break;
+      case 'Indian':
+        cuisineImage = './assets/images/indian.jpg';
+        break;
+      case 'Italian':
+        cuisineImage = './assets/images/italian.jpg';
+        break;
+      case 'Tibetan':
+        cuisineImage = './assets/images/tibetan.jpg';
+        break;
+      case 'Mongolian':
+        cuisineImage = './assets/images/mongolian.jpg';
+        break;
+      case 'Indonesian':
+        cuisineImage = './assets/images/indonesian.jpg';
+        break;
+      case 'Mediterranean':
+        cuisineImage = './assets/images/mediterranean2.jpg';
+        break;
+      case 'Korean':
+        cuisineImage = './assets/images/korean.jpg';
+        break;
+      case 'Indian (Southern)':
+        cuisineImage = './assets/images/indiansouthern.jpg';
+        break;
+      case 'Middle Eastern/Persian':
+        cuisineImage = './assets/images/middleeastern.jpg';
+        break;
+      case 'Vietnamese':
+        cuisineImage = './assets/images/vietnamese.jpg';
+        break;
+      case 'Szechuan':
+        cuisineImage = './assets/images/szechuan.jpg';
+        break;
+      case 'Greek':
+        cuisineImage = './assets/images/greek.jpg';
+        break;
+      case 'Thai':
+        cuisineImage = './assets/images/thai.jpg';
+        break;
+      case 'African':
+        cuisineImage = './assets/images/african1.jpg';
+        break;
+      default:
+        console.log('Sorry, we dont have ' + favorites.cuisine + ' in our system yet.');
+        cuisineImage = './assets/images/vegetarian1.jpg';
+    }
+
     // Build card using jQuery
     let favoriteCardElement = $("<div>").addClass("col s12 m6 l4")
       .append($("<div>").addClass("card")
         .append($("<div>").addClass("card-image")
-          .append($("<img>").attr("src", "./assets/images/pizza.jpg"))
+          .append($("<img>").attr("src", cuisineImage).attr("alt", "Image of cuisine type"))
           .append($("<span>").addClass("card-title").text(favorites.name))
         )
         .append($("<div>").addClass("card-content")
@@ -28,7 +105,7 @@ $(document).ready(function () {
             .append($("<img>").attr("id", "redheart").attr("src", "assets/images/redheart.png"))
           )
         )
-        .append($("<div>").addClass("card-action").attr("style", "overflow:scroll")
+        .append($("<div>").addClass("card-action").attr("style", "overflow-wrap:break-word")
           .append($("<a>").attr("href", favorites.website).attr("style", "text-transform:none").text(favorites.website))
         )
       )
@@ -55,17 +132,18 @@ $(document).ready(function () {
   // Function to update user favorites 
   $(document).on('click', '#redheart', function () {
     // Get userId
-    var userId = firebase.auth().currentUser.uid;
+    let userId = firebase.auth().currentUser.uid;
     // Grab the elememt user wants deleted
     let favoriteItem = $(this).parent();
-    favoriteItem = favoriteItem[0].offsetParent.childNodes[0].innerText;
+    let keyToRemove = favoriteItem[0].offsetParent.childNodes[0].innerText;
 
     // Remove favorite from database
-    return firebase.database().ref('/users/' + userId + '/favorites/' + favoriteItem).remove()
-    .then(function() {
-      // Reload page so favorites can update
-      location.reload(true);
-    });
+    return firebase.database().ref('/users/' + userId + '/favorites/' + keyToRemove).remove()
+      .then(function () {
+        // Hide card
+        let cardToBeRemoved = favoriteItem[0].parentElement.parentElement.parentElement;
+        $(cardToBeRemoved).attr("style", "display:none");
+      });
   });
 
 
